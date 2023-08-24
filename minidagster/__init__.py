2d02593746d83af77e7cfaa4cfdc05fb16a5407e
Joao Meyer <1994meyer@gmail.com>
2023-08-24 20:42:42 +0100

    New example

--- minidagster/__init__.py
+++ minidagster/__init__.py
@@ -6,6 +6,7 @@ warnings.filterwarnings('ignore', category=ExperimentalWarning)
 from . import (
     linear_with_partitions, 
     linear_without_partitions,
+    mapping_multi_dynamic_partitions,
     parent_with_partitions,
     parent_without_partitions,
     diamond_with_partitions,
@@ -14,55 +15,66 @@ from . import (
 )
 
 
-linear_with_partitions = load_assets_from_modules(
+linear_with_partitions_assets = load_assets_from_modules(
     modules=[linear_with_partitions],
     key_prefix='linear_with_partitions',
     group_name='linear_with_partitions',
 )
 
-linear_without_partitions = load_assets_from_modules(
+linear_without_partitions_assets = load_assets_from_modules(
     modules=[linear_without_partitions],
     key_prefix='linear_without_partitions',
     group_name='linear_without_partitions',
 )
 
-parent_with_partitions = load_assets_from_modules(
+parent_with_partitions_assets = load_assets_from_modules(
     modules=[parent_with_partitions],
     key_prefix='parent_with_partitions',
     group_name='parent_with_partitions',
 )
 
-parent_without_partitions = load_assets_from_modules(
+parent_without_partitions_assets = load_assets_from_modules(
     modules=[parent_without_partitions],
     key_prefix='parent_without_partitions',
     group_name='parent_without_partitions',
 )
 
-diamond_with_partitions = load_assets_from_modules(
+diamond_with_partitions_assets = load_assets_from_modules(
     modules=[diamond_with_partitions],
     key_prefix='diamond_with_partitions',
     group_name='diamond_with_partitions',
 )
 
-diamond_mixed = load_assets_from_modules(
+diamond_mixed_assets = load_assets_from_modules(
     modules=[diamond_mixed],
     key_prefix='diamond_mixed',
     group_name='diamond_mixed',
 )
 
+mapping_multi_dynamic_partitions_assets = load_assets_from_modules(
+    modules=[mapping_multi_dynamic_partitions],
+    key_prefix='mapping_multi_dynamic_partitions',
+    group_name='mapping_multi_dynamic_partitions',
+)
+
 
 assets = []
-assets += linear_with_partitions
-assets += linear_without_partitions
-assets += parent_with_partitions
-assets += parent_without_partitions
-assets += diamond_with_partitions
-assets += diamond_mixed
+assets += linear_with_partitions_assets
+assets += linear_without_partitions_assets
+assets += parent_with_partitions_assets
+assets += parent_without_partitions_assets
+assets += diamond_with_partitions_assets
+assets += diamond_mixed_assets
+assets += mapping_multi_dynamic_partitions_assets
 
 
 defs = Definitions(
     assets=assets,
+    sensors=[
+        mapping_multi_dynamic_partitions.random_docs_sensor,
+        mapping_multi_dynamic_partitions.random_grouped_sensor,
+    ],
     resources={
         'noop': resources.noop,
-    }
+    },
 )
---
+++ minidagster/mapping_multi_dynamic_partitions.py
@@ -0,0 +1,47 @@
+import random
+
+from dagster import *
+
+
+docs = DynamicPartitionsDefinition(name='docs')
+grouped = DynamicPartitionsDefinition(name='grouped')
+partition = MultiPartitionsDefinition({'grouped': grouped, 'docs': docs})
+
+
+@sensor()
+def random_docs_sensor():
+    """ Dummy sensor to create random partitions """
+    keys = [f'{i:03d}' for i in range(10)]
+    requests = docs.build_add_request(keys)
+    return SensorResult(dynamic_partitions_requests=[requests])
+
+
+@sensor()
+def random_grouped_sensor():
+    """ Dummy sensor to create random partitions """
+    keys =['a', 'b', 'c']
+    requests = grouped.build_add_request(keys)
+    return SensorResult(dynamic_partitions_requests=[requests])
+
+
+@asset(
+    partitions_def=partition,
+)
+def g_first(context: OpExecutionContext) -> str:
+    context.log.info("%s", context.partition_key)
+    return context.partition_key
+
+
+@asset(
+    partitions_def=grouped,
+    ins={
+        'val': AssetIn(
+            key=g_first.key,
+            partition_mapping=MultiToSingleDimensionPartitionMapping('grouped'),
+        ),
+    },
+    auto_materialize_policy=AutoMaterializePolicy.eager(),
+)
+def g_second(context: OpExecutionContext, val: dict) -> dict:
+    context.log.info("%s", val)
+    return val
